39b2af782c660b254800d6238171890a992b32b5
David Roy <david.roy@codebunny.scot>
2019-01-06 20:52:22 +0000

    adds interactive messages

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :chat_rooms, path: '/chat-rooms', only: [:new, :create, :show, :index]
 
   devise_for :users
+  mount ActionCable.server => '/cable'
   root 'welcome#index'
 end
